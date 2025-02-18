a2a6ca4cf57b046aa713d65c08844a3513203698
eshwar4202 <eshwar4202@gmail.com>
2025-02-18 09:24:56 +0530

    login

--- App.tsx
+++ App.tsx
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react'
+import { supabase } from './lib/supabase'
+import Auth from './components/Auth'
+import Account from './components/Account'
+import { View, StyleSheet, Text } from 'react-native'
+import { Session } from '@supabase/supabase-js'
 
 export default function App() {
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style = {styles.centered}>
+      <Text style ={styles.header}>Login</Text>
+      {session && session.user ? <Account key={session.user.id} session={session}/> : <Auth />}
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  centered: {
+    justifyContent: "center",
+    top : 150,
   },
+  header: {
+    fontWeight: 'bold',
+    fontSize: 30,
+    left: 20,
+  }
 });
